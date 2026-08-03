eff30b8ed6bac72dde46af660990ff2ec8dd025f
Hashir Hamza <64621908+hashirhamxa@users.noreply.github.com>
2026-08-03 17:56:50 +0500

    Update app launcher icons to new adaptive, rounded, and flat variants, and increment version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "livecricket.livecrickettv.cricketstreaming"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.2"
 
         buildConfigField("String", "API_TOKEN", "\"$apiToken\"")
 
