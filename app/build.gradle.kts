a0a24a82c131ccff59b9552b4bfe7f5513b93fab
Hashir Hamza <64621908+hashirhamxa@users.noreply.github.com>
2026-08-10 22:13:08 +0500

    versioncode 6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "livecricket.livecrickettv.cricketstreaming"
         minSdk = 24
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.5"
+        versionCode = 6
+        versionName = "1.6"
 
         buildConfigField("String", "API_TOKEN", "\"$apiToken\"")
 
